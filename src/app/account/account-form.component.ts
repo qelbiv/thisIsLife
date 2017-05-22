dce5cc247e5c2bf9b5ebff01a5fb5813ff928b86
sedupuis <sebastiengt.dupuis@gmail.com>
2017-05-22 22:32:42 +0200

    travail du soir

--- src/app/account/account-form.component.ts
+++ src/app/account/account-form.component.ts
@@ -12,13 +12,13 @@ import {User}       from './Users';
 })
 
 export class AccountFormComponent (mail: any,pseudo: any,familyname: any,name: any){
-   
+    
     
     private user = new User(this.name, this.familyname, this.pseudo, this.mail);
     
     onSubmit(): void {
         console.log(this.user); 
-    
+        
     })
 }
 
---
+++ src/app/mapview/menu.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-menu',
+    templateUrl: 'menu.component.html'
+})
+
+export class MenuComponent{
+    
+}
