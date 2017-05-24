fc6106dfcc754da92da278a45e63e91faf1e6998
MikaLTX <mikaletoux@gmail.com>
2017-05-24 11:52:57 +0200

    le changement de la mort qui tue

--- src/app/account/form.component.ts
+++ src/app/account/form.component.ts
@@ -5,21 +5,16 @@ import {User}       from './user';
     moduleId : module.id,
     selector: 'form',
     templateUrl: './form.component.html',
-    styles : 
-        [`
-    .ng-valid{border-color: green;  }
-    .ng-invalid{border-color: red;  }
-        `]
 })
 
 export class FormComponent {
  
     user = {};
     
-    onSubmit(value:any): void {
-        let user = new User(value.name, value.familyname, value.pseudo, value.mail);
-        console.log(user); 
-    }
+//    onSubmit(value:any): void {
+//        let user = new User(value.name, value.familyname, value.pseudo, value.mail);
+//        console.log(user); 
+//    }
 }
         
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { MapComponent } from "./mapview/map.component";
 
-import {AccountComponent} from './account/account.component';
 import {FormComponent} from './account/form.component';
 
 
